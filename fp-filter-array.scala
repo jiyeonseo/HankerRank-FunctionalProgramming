3c68d227a59eb65e255a1a0ffbf8d09d38e1a5ad
jiyeonseo <seojeee@gmail.com>
2017-06-07 19:42:00 +0900

    solve "fp-filter-array"

---
+++ fp-filter-array.scala
@@ -0,0 +1,6 @@
+// https://www.hackerrank.com/challenges/fp-filter-array
+
+def f(delim:Int,arr:List[Int]):List[Int] = arr.filter(_ < delim)
+
+// filter에 대한 자세한 설명 https://twitter.github.io/scala_school/ko/collections.html#filter
+
